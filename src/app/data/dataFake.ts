bbc3e746ae08535629209f257afe0bf07d53e4a9
Phelipe Pereira <phelipe.pereira0811@hotmail.com>
2023-10-04 18:35:36 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title":"NOVO HOMEM DE FERRO FOI ANUNCIADO",
+    "description": "Marvel studios anuncia novo filme do homem de ferro",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id": "2",
+    "title":"Nova Séria da Marvel foi lançada.",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies ornare diam, ac imperdiet ligula placerat ut. Praesent et nibh dolor. Praesent dignissim erat et purus commodo viverra. Nulla diam purus, tincidunt sit amet facilisis ac, condimentum non ex. Nullam sodales finibus dignissim. Fusce eu ex ut mi aliquam commodo non eget lectus.",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id": "3",
+    "title":"Pantera negra é uma dos personagens mais comentados atualmente!",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies ornare diam, ac imperdiet ligula placerat ut. Praesent et nibh dolor. Praesent dignissim erat et purus commodo viverra. Nulla diam purus, tincidunt sit amet facilisis ac, condimentum non ex. Nullam sodales finibus dignissim. Fusce eu ex ut mi aliquam commodo non eget lectus.",
+    "photoCover": "https://gamehall.com.br/wp-content/uploads/2022/07/pantera-negra.jpg"
+  },
+  {
+    "id": "4",
+    "title":"Marvel e Capcom planejam lançar novo jogo de luta.",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies ornare diam, ac imperdiet ligula placerat ut. Praesent et nibh dolor. Praesent dignissim erat et purus commodo viverra. Nulla diam purus, tincidunt sit amet facilisis ac, condimentum non ex. Nullam sodales finibus dignissim. Fusce eu ex ut mi aliquam commodo non eget lectus.",
+    "photoCover": "https://4.bp.blogspot.com/-YubYyS6ox8c/WdGXCsmXHTI/AAAAAAAAEP8/2H0Otdvs9MgLXvNYQkKJbpB4Xy9coD6wgCLcBGAs/s1600/Marvel-Vs-Capcom.jpg"
+  }
+]
